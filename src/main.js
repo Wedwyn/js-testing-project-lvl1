288cde1ea3bc08a29c5a999d95ab698dde2dd3f9
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-13 22:32:52 +0300

    removed the work with the console to another file

--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
-import { Command } from 'commander';
 import createFileName from './createFileName.js';
 import savePageInDirectory from './SavePageInDirectory.js';
 import getPageFromUrl from './getPageFromUrl.js';
 
 
-const program = new Command();
+const main = (url, path) => {
+    console.log(`Page was successfully downloaded into: ${path}`);
+    const modifiedPath = `${path}/${createFileName(url)}`;
+    getPageFromUrl(url).then(result => savePageInDirectory(modifiedPath, result.data));
+}
 
-program // работа с Commander.js
-    .name('page-loader')
-    .description('Download page from website')
-    .version('0.0.1')
-    .argument('<url>', 'string')
-    .option('-o, --output [dir]', `output dir`, '/home/wedwyn/test/downloads')
-    .action((url, options) => {
-        const path = options.output;
-        console.log(`This is path: ${path}`);
-        const modifiedPath = `${path}/${createFileName(url)}`;
-        getPageFromUrl(url).then(result => savePageInDirectory(modifiedPath, result.data));
-    });
-
-program.parse(process.argv);
-
+export default main;
