5ccb364bfe9b32c01eb000714b18fbdb5b4238d3
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-13 22:31:00 +0300

    fixed the function

--- src/createFileName.js
+++ src/createFileName.js
@@ -1,10 +1,10 @@
 const createFileName = (url) => {
     const urlWithoutProtocol =  url.split('://').slice(1).join('');
     let modifiedUrl = '';
+    const allForbiddenSymbols = `!@#$%&*()+="';:{}[]~/.,`;
     for (let i = 0; i < urlWithoutProtocol.length; i +=1) {
         let currentSymbol = urlWithoutProtocol[i];
-        if (currentSymbol === '.' || currentSymbol === '/'
-        || currentSymbol === '_' || currentSymbol === '?') {
+        if (allForbiddenSymbols.indexOf(currentSymbol) !== -1) {
             currentSymbol = '-';
         }
         modifiedUrl += currentSymbol;
