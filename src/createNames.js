71862f124bfb81cedffd88aa664f7e99d3028912
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-17 00:08:28 +0300

    add create functions for image and directory

---
+++ src/createNames.js
@@ -0,0 +1,46 @@
+const createFileName = (url) => {
+    const urlWithoutProtocol =  url.split('://').slice(1).join('');
+    let modifiedUrl = '';
+    const allForbiddenSymbols = `!@#$%&*()+="';:{}[]~/.,`;
+    for (let i = 0; i < urlWithoutProtocol.length; i +=1) {
+        let currentSymbol = urlWithoutProtocol[i];
+        if (allForbiddenSymbols.indexOf(currentSymbol) !== -1) {
+            currentSymbol = '-';
+        }
+        modifiedUrl += currentSymbol;
+    }
+    return `${modifiedUrl}.html`;
+}
+
+const createDirectoryName = (url) => {
+    const urlWithoutProtocol =  url.split('://').slice(1).join('');
+    let modifiedUrl = '';
+    const allForbiddenSymbols = `!@#$%&*()+="';:{}[]~/.,`;
+    for (let i = 0; i < urlWithoutProtocol.length; i +=1) {
+        let currentSymbol = urlWithoutProtocol[i];
+        if (allForbiddenSymbols.indexOf(currentSymbol) !== -1) {
+            currentSymbol = '-';
+        }
+        modifiedUrl += currentSymbol;
+    }
+    return `${modifiedUrl}_files`;
+}
+
+const createImageName = (url) => {
+    let urlWithoutProtocol = url.slice(0, url.lastIndexOf('.'));
+    if (urlWithoutProtocol.indexOf('://') !== -1) {
+        urlWithoutProtocol =  url.slice(0, url.lastIndexOf('.')).split('://').slice(1).join('');
+    }
+    let modifiedUrl = '';
+    const allForbiddenSymbols = `!@#$%&*()+="';:{}[]~/.,`;
+    for (let i = 0; i < urlWithoutProtocol.length; i +=1) {
+        let currentSymbol = urlWithoutProtocol[i];
+        if (allForbiddenSymbols.indexOf(currentSymbol) !== -1) {
+            currentSymbol = '-';
+        }
+        modifiedUrl += currentSymbol;
+    }
+    return `${modifiedUrl}${url.slice(url.lastIndexOf('.'))}`;
+}
+
+export {createFileName, createDirectoryName, createImageName};
