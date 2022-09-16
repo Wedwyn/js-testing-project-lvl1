8d2e3ca28d8b13dd962cf5737dd2fc5e84f8c9ba
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-16 23:59:34 +0300

    changed the default path

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -8,9 +8,9 @@ const program = new Command();
 program // работа с Commander.js
     .name('page-loader')
     .description('Download page from website')
-    .version('0.0.1')
+    .version('0.1.0')
     .argument('<url>', 'string')
-    .option('-o, --output [dir]', `output dir`, '/home/wedwyn/test/downloads')
+    .option('-o, --output [dir]', `output dir`, process.cwd())
     .action((url, options) => main(url, options.output));
 
 
