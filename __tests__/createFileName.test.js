5c7a6de127e575cc36611a14c1d382f40cb25dca
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-19 22:21:32 +0300

    moved all naming tests in createNames.test.js

--- __tests__/createFileName.test.js
+++
@@ -1,9 +0,0 @@
-import createFileName from '../src/createFileName.js';
-
-test('createFileName test one', () => {
-    expect(createFileName('https://www.kinopoisk.ru')).toBe('www-kinopoisk-ru.html');
-    expect(createFileName('https://ru.hexlet.io/courses')).toBe('ru-hexlet-io-courses.html');
-    expect(createFileName('https://www.youtube.com/shorts/2ERjw5VFbaU')).toBe('www-youtube-com-shorts-2ERjw5VFbaU.html');
-});
-
-// NODE_OPTIONS=--experimental-vm-modules npx jest
