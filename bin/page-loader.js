92c6697c6dd4baf5a5f8091d0aa7da3d12cd15f1
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-24 00:16:27 +0300

    turned off linter for two lines

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 import main from "../src/main.js";
 
@@ -10,8 +9,8 @@ program // работа с Commander.js
     .description('Download page from website')
     .version('0.1.0')
     .argument('<url>', 'string')
-    .option('-o, --output [dir]', `output dir`, process.cwd())
+    .option('-o, --output [dir]', `output dir`, process.cwd()) // eslint-disable-line
     .action((url, options) => main(url, options.output));
 
 
-program.parse(process.argv);
+program.parse(process.argv); // eslint-disable-line
