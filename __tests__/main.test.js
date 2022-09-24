06cd4212ad52064efe99c89634594b6849ab8cfa
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-24 19:02:01 +0300

    added check to save all local files

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -20,8 +20,13 @@ afterEach(async () => {
 });
 
 test('test main.js', async () => {
-    const answer = await readFile(`${__dirname}/filesForTest/bodyForTestMain.txt`);
+    const answer = await readFile(`${__dirname}/filesForTest/bodyForTestMain.txt`, 'utf-8');
+    const cssFile = await readFile(`${__dirname}/filesForTest/cssForTest.txt`, 'utf-8');
+    const scriptFile = await readFile(`${__dirname}/filesForTest/scriptForTest.txt`, 'utf-8');
+    const localFileInMain = await readFile(`${__dirname}/filesForTest/LocalFileInMainFile.txt`, 'utf-8');
+    // console.log(localFileInMain);
     const scope = nock('https://page-loader.hexlet.repl.co/')
+    .persist()
     .get('/')
     .reply(200, answer, {
         'Content-Type': 'text/html',
@@ -29,12 +34,24 @@ test('test main.js', async () => {
     .get('/assets/professions/nodejs.png')
     .replyWithFile(200, `${__dirname}/filesForTest/image.png`, {
         'Content-Type': 'image/png'
+    })
+    .get('/assets/application.css')
+    .reply(200, cssFile, {
+        'Content-Type': 'text/css',
+    })
+    .get('/script.js')
+    .reply(200, scriptFile, {
+        'Content-Type':  'text/js',
+    })
+    .persist()
+    .get('/courses')
+    .reply(200, localFileInMain, {
+        'Content-Type': 'text/html',
     });
+
     await main('https://page-loader.hexlet.repl.co/', `${__dirname}${tempDirectoryName}`);
     const files = await readdir(`${__dirname}${tempDirectoryName}/page-loader-hexlet-repl-co-_files`);
-    const countOfHtmlFiles = files.filter((el) => el.split(el.lastIndexOf('.') === 'html')).length;
-    const countOfImageFiles = files.filter((el) => el.split(el.lastIndexOf('.') === 'png')).length;
-    expect(countOfHtmlFiles+countOfImageFiles).toBe(2);
+    expect(files.length).toBe(5);
     scope.done();
 
 });
