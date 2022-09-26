f5e00af83210bafdc6a64afe76a65f653c5f8923
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-09-26 23:24:51 +0300

    rename function which is being tested

--- __tests__/createNames.test.js
+++ __tests__/createNames.test.js
@@ -1,4 +1,4 @@
-import {createFileName, createDirectoryName, createImageName} from '../src/createNames.js';
+import {createFileName, createDirectoryName, createResourceName} from '../src/createNames.js';
 
 test('createFileName', () => {
     expect(createFileName('https://www.kinopoisk.ru')).toBe('www-kinopoisk-ru.html');
@@ -12,10 +12,10 @@ test('createDirectoryName', () => {
     expect(createDirectoryName('https://www.youtube.com/shorts/2ERjw5VFbaU')).toBe('www-youtube-com-shorts-2ERjw5VFbaU_files');
 });
 
-test('createImageName', () => {
-    expect(createImageName('/images/prev/9bbd2aae400b961b2858977fb2ffdf3f_w200.png')).toBe('-images-prev-9bbd2aae400b961b2858977fb2ffdf3f-w200.png');
-    expect(createImageName('/pub#lic/avatars/avat*ar_92080.jpg')).toBe('-pub-lic-avatars-avat-ar-92080.jpg');
-    expect(createImageName('/wwwdata/www/skin/pusto.gif')).toBe('-wwwdata-www-skin-pusto.gif');
+test('createResource', () => {
+    expect(createResourceName('/images/prev/9bbd2aae400b961b2858977fb2ffdf3f_w200.png')).toBe('-images-prev-9bbd2aae400b961b2858977fb2ffdf3f-w200.png');
+    expect(createResourceName('/pub#lic/avatars/avat*ar_92080.jpg')).toBe('-pub-lic-avatars-avat-ar-92080.jpg');
+    expect(createResourceName('/wwwdata/www/skin/pusto.gif')).toBe('-wwwdata-www-skin-pusto.gif');
 });
 
 
